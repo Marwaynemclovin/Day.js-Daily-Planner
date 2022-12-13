4a89c2496f03e3656777d90e3bd8e6b4c74571de
Marwin Manlangit <marwin.m@hotmail.com>
2022-12-13 12:01:31 +1100

    Initial Commit, added basic JS, completed date updater

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,27 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('dddd D/MMMM/YYYY');
+  
+  $('#currentDay').text(currentDate);
+
+
+  $(".saveBtn").click(function(){
+    var time = $(this).parent().attr('id');
+    var description = $(this).parent().children('textarea').val();
+
+    localStorage.setItem(time, description);
+  })
+
+
+var currentHour = moment().hour();
+
+$('.time-block').each(function(){
+  
+})
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -19,5 +40,5 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
